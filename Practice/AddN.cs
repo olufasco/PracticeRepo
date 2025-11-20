1b998fc323158a85924ef989b3f7166a3818ae3f
olufasco <olufasco1@gmail.com>
2025-11-20 23:36:04 +0100

    thursday late commit

---
+++ Practice/AddN.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Practice
+{
+    public class TwistedSum
+    {
+        public static long Solution(long n)
+        {
+            long result = 0;
+            for (int i = 1; i <= n; i++)
+            {
+                if (i.ToString().Length == 1)
+                {
+                    result += i;
+                }
+                else
+                {
+                    foreach (char a in i.ToString())
+                        result += (long)Char.GetNumericValue(a);
+                }
+            }
+            return result;
+        }
+    }
+}
--- Practice/Program.cs
+++ Practice/Program.cs
@@ -1,6 +1,12 @@
 ﻿using Practice;
+using System.ComponentModel;
 
 //Console.WriteLine(Fasco.CheckIfOdd(1, 5, 1));
 //Console.WriteLine(Fasco.GetCharAtIndex("muhydeen", 8));
-//Console.WriteLine(Roman.IntToRoman(1526));
-Console.WriteLine(Solution.TwoSum(new int[] { 2, 3, 9, 8, 5, 1}, 6));
+//Console.WriteLine(Roman.IntToRoman(754));
+//Console.WriteLine(Solution.TwoSum(new int[] { 2, 3, 9, 8, 5, 1}, 6));
+//Console.WriteLine(TwistedSum.Solution(10));
+//Console.WriteLine(NewClass.GetProduct(3,80));
+//Console.WriteLine(ReturnList.GetList("the", "boy", "name"));
+//Console.WriteLine(SecondOne.NewList(4,6,8));
+Console.WriteLine(FourthOne.Bigger(5, 5));
